c1fc6c5875cb7f97d67a0edd09d98bcbeff2ed29
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-06-14 16:13:00 +0200

    component created, title succesfully shown on page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import AppTitle from './components/AppTitle.vue'
 </script>
 
 <template>
@@ -11,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppTitle/>
 </template>
 
 <style scoped>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>
+        {{ title }}
+    </h1>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                title: "La mia prima app con Vite!"
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
